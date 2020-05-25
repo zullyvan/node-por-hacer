7137b5884011745cb85df45c5ebd88c44988a338
Zullyvan <Zullyvan@gmail.com>
2020-05-24 20:48:14 -0500

    Primer commit

---
+++ README.md
@@ -0,0 +1,7 @@
+##  Aplicacion de comandos
+
+Este es el ejercicio del curso
+
+Recurden instalar los paquetes de node
+
+npm install
---
+++ config/yargs.js
@@ -0,0 +1,29 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripcion de la tarea por hacer'    
+};
+
+const completado = {
+    default: true,
+    alias: 'c',
+    desc: 'Marca como completado o pendiente la tarea'
+}
+
+const argv = require('yargs')
+    .command('crear', 'Crear un elemento por hacer', {
+        descripcion
+    })
+    .command('actualizar', 'Actualiza el estado completado de la tarea', {
+        descripcion,
+        completado
+    })
+    .command('borrar', 'Borrar un elemento por hacer', {
+        descripcion
+    })     
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
